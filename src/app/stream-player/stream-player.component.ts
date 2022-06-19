d4dd7a2f5095fc979c6b360e7c147401c5ea8237
Emilio Cortés <emilio.cortes@decemberlabs.com>
2022-06-19 03:31:30 -0300

    Mute local audio

--- src/app/stream-player/stream-player.component.ts
+++ src/app/stream-player/stream-player.component.ts
@@ -8,11 +8,18 @@ import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '
 export class StreamPlayerComponent implements OnInit, AfterViewInit {
   @Input()
   stream!: MediaStream;
+
+  @Input()
+  muted: boolean = false;
+
   @ViewChild('videoContainer')
   videoContainer!: ElementRef;
   constructor() {}
   ngAfterViewInit(): void {
     this.videoContainer.nativeElement.srcObject = this.stream;
+    if (this.muted) {
+      this.videoContainer.nativeElement.muted = true;
+    }
     this.videoContainer.nativeElement.play();
   }
 
